5561e8cd1411ec8855153973bafbc1db8b82e9d8
August <2819763+Dangku@users.noreply.github.com>
2025-06-30 17:07:13 +0800

    update for yocto build

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --privileged -it -v /media:/media -v /etc/localtime:/etc/localtime -v /dev:/dev -v /proc:/proc -v /tmp:/tmp sinovoip/bpi-build-v2h-yocto:ubuntu20.04 /bin/bash
+docker run --privileged --pid=host -it -v /media:/media -v /etc/localtime:/etc/localtime -v /dev:/dev -v /proc:/proc -v /tmp:/tmp sinovoip/bpi-build-v2h-yocto:ubuntu20.04 /bin/bash
